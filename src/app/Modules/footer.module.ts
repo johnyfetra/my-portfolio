0528afdfe55ea276374da003c087315d04ed72ff
fetra johny <fetrajohny05@gmail.com>
2024-04-26 18:37:52 +0300

    add layout

---
+++ src/app/Modules/footer.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../Components/Layouts/footer/footer.component';
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [CommonModule],
+  exports: [FooterComponent],
+})
+export class FooterModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './Components/Layouts/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+];
